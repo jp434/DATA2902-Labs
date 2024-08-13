167579a53c55fb4b7f0ffad16d0b803d9fc0bf5f
jp434 <jasonphan37@gmail.com>
2024-08-13 13:47:56 +1000

    Commit from RStudio 13/8

--- app.R
+++ app.R
@@ -10,6 +10,8 @@
 library(shiny)
 library(tidyverse)
 library(palmerpenguins)
+library(DT)
+library(gt)
 
 numeric_choices = c("body_mass_g", "flipper_length_mm", "bill_depth_mm", "bill_length_mm")
 categorical_choices = c("species", "island", "sex", "year")
@@ -30,7 +32,7 @@ ui <- fluidPage(
             selectInput(inputId = "var_xaxis", 
                         label = "X axis variable", 
                         choices = numeric_choices, 
-                        selected = "flipper_length_mm"),
+                        selected = "body_mass_g"),
             selectInput(inputId = "var_yaxis", 
                         label = "Y axis variable", 
                         choices = numeric_choices, 
@@ -42,12 +44,18 @@ ui <- fluidPage(
             selectInput(inputId = "var_facet", 
                         label = "Faceting variable", 
                         choices = categorical_choices, 
-                        selected = "island") 
+                        selected = "island"),
+            checkboxInput(inputId = "checkbox_line_of_fit", label = 'Enable line of best fit', value = T)
         ),
 
         # Show a plot of the generated distribution
         mainPanel(
-           plotOutput("distPlot")
+          tabsetPanel(
+            tabPanel(title = 'Main Plot', 
+              plotOutput("distPlot"), gt::gt_output(outputId = 'corr_table')
+            ),
+            tabPanel(title = 'Penguins Table', DT::DTOutput(outputId = 'table_simple'))
+          )
         )
     )
 )
@@ -62,7 +70,18 @@ server <- function(input, output) {
             y = .data[[input$var_yaxis]],
             colour = .data[[input$var_col]]) +
         geom_point() +
-        facet_grid(cols = vars(.data[[input$var_facet]]))
+        facet_grid(cols = vars(.data[[input$var_facet]])) + 
+        {if(input$checkbox_line_of_fit == T) geom_smooth(method = 'lm', se = F)}
+    })
+    
+    output$table_simple <- renderDT({
+      penguins
+    })
+    
+    output$corr_table <- render_gt({
+      cor_table = penguins %>%
+        group_by(.data[[input$var_facet]]) %>% 
+        summarise(r = cor(.data[[input$var_xaxis]], .data[[input$var_yaxis]], use = "pairwise.complete.obs"))
     })
 }
 
